7f696f531617a821e4e337f7b676ab55c5ee9849
Colton M <colton.adkins56@yahoo.com>
2016-11-22 15:06:02 -0800

    It's hard coded but technically works, let me know your thoughts

--- myapp.rb
+++ myapp.rb
@@ -1,15 +1,23 @@
 require 'sinatra'
 require 'json'
 
-get '/example' do
+enable :sessions
+
+get '/todos' do
   content_type :json
-  result = [
-      'this', 'is', 'a', 'list'
-  ]
-  result.to_json
+  session[:list] = ['paint the barn', 'water the corn', 'feed the hogs', 'fix the car']
+  session[:list].to_json
+
 end
 
-get '/another_example' do
+post '/todos' do
   content_type :json
-  {a: 1, b: 2, c: 3}.to_json
+  session[:list] = ['paint the barn', 'water the corn', 'feed the hogs', 'fix the car', 'wash the dog']
+  session[:list].to_json
+end
+
+delete '/todos' do
+  content_type :json
+  session[:list] = ['feed the hogs', 'fix the car', 'wash the dog']
+  session[:list].to_json
 end
--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,10 +1,27 @@
 require 'rest-client'
+require 'json'
 
-describe 'the example services' do
+
+describe 'gets the list' do
   it 'has a basic GET service I can call' do
-    response = RestClient.get 'http://localhost:4567/another_example', :accept => :json
+    response = RestClient.get 'http://localhost:4567/todos', :accept => :json
     content = JSON.parse(response,:symbolize_names => true)
-    expect(content.size).to eq 3
-    expect(content[:a]).to eq 1
+    expect(content.size).to eq 4
+  end
+end
+
+describe 'adds stuff to the list' do
+  it 'has a POST service I can call' do
+    response2 = RestClient.post 'http://localhost:4567/todos', :accept => :json
+    content2 = JSON.parse(response2, :symbolize_names => true)
+    expect(content2.size).to eq 5
+  end
+end
+
+describe 'removes stuff from the list' do
+  it 'has a DELETE service I can call' do
+    response3 = RestClient.delete 'http://localhost:4567/todos', :accept => :json
+    content3 = JSON.parse(response3, :symbolize_names => true)
+    expect(content3.size).to eq 3
   end
 end
