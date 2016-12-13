e4f195941a0e4074766d7d2d4e93a3f116d67340
Tim Glover <tglover9@student.cscc.edu>
2016-12-12 22:36:10 -0500

    'updated'

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,15 +1,7 @@
 require 'rest-client'
 require 'json'
 
-=begin
-describe 'the example services' do
-  it 'has a basic GET service I can call' do
-    response = RestClient.get 'http://localhost:4567/another_example', :accept => :json
-    content = JSON.parse(response,:symbolize_names => true)
-    expect(content.size).to eq 3
-    expect(content[:a]).to eq 1
-  end
-=end
+
 
 describe 'the to do list' do
   it 'has a list that i can view and edit' do
@@ -31,7 +23,6 @@ describe 'the to do list' do
     expect(item_4.last).to eq 'pay the bills'
 
 
-    response = RestClient.get 'http://localhost:4567/todo_list', :accept => :json
     RestClient.delete 'http://localhost:4567/todo_list/3', :accept => :json, :symbolize_names =>true
     item_5 = JSON.parse(RestClient.get 'http://localhost:4567/todo_list', :accept => :json, :symbolize_names =>true)
     expect(item_5.size).to eq (item_4.length - 1)
